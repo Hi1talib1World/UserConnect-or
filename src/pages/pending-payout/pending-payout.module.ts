b029a0a846282979f7f9519be4e075b40390b21c
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-14 23:18:54 +0100

    Update pending-payout.module.ts

---
+++ src/pages/pending-payout/pending-payout.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { PendingPayoutPage } from './pending-payout';
+import { TranslateModule } from "@ngx-translate/core";
+
+@NgModule({
+    declarations: [
+        PendingPayoutPage,
+    ],
+    imports: [
+        IonicPageModule.forChild(PendingPayoutPage),
+        TranslateModule.forChild()
+    ],
+})
+export class PendingPayoutPageModule {}
