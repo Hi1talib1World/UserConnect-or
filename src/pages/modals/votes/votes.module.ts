18f97d1c67160615bb2977d67c60e1898a6d10af
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-15 23:21:53 +0100

    Update votes.module.ts

---
+++ src/pages/modals/votes/votes.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { VotesPage } from './votes';
+import { ComponentsModule } from '../../../components/components.module';
+import { TranslateModule } from "@ngx-translate/core";
+
+@NgModule({
+    declarations: [
+        VotesPage,
+    ],
+    imports: [
+        ComponentsModule,
+        IonicPageModule.forChild(VotesPage),
+        TranslateModule.forChild()
+    ],
+})
+export class VotesPageModule {}
