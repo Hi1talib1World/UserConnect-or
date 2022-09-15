ded6f94291214fa18be41fa651b27a442416f265
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-15 21:20:33 +0100

    Update edit-comment.module.ts

---
+++ src/pages/edit-comment/edit-comment.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { EditCommentPage } from './edit-comment';
+import { TranslateModule } from "@ngx-translate/core";
+
+@NgModule({
+    declarations: [
+        EditCommentPage,
+    ],
+    imports: [
+        IonicPageModule.forChild(EditCommentPage),
+        TranslateModule.forChild()
+    ],
+})
+export class EditCommentPageModule {}
