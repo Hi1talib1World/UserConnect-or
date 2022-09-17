370a304d5e82df8ed75347cd13ef4d052e860d64
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-17 21:15:50 +0100

    Update reply-comment.module.ts

---
+++ src/pages/modals/reply-comment/reply-comment.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ReplyCommentPage } from './reply-comment';
+import { TranslateModule } from "@ngx-translate/core";
+
+@NgModule({
+    declarations: [
+        ReplyCommentPage,
+    ],
+    imports: [
+        IonicPageModule.forChild(ReplyCommentPage),
+        TranslateModule.forChild()
+    ],
+})
+export class ReplyCommentPageModule {}
