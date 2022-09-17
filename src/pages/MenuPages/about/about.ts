d45aeabb888b0c2c6181e6cc036e78f5dcbd8f1b
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-17 20:55:37 +0100

    Update about.ts

---
+++ src/pages/MenuPages/about/about.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { IonicPage, MenuController } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+    selector: 'page-about',
+    templateUrl: 'about.html',
+})
+export class AboutPage {
+
+    constructor(public menu: MenuController) {
+    }
+
+    ionViewDidEnter() {
+        this.menu.enable(false);
+    }
+
+    ionViewWillLeave() {
+        this.menu.enable(true);
+    }
+
+}
