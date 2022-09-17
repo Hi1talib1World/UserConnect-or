5d6f41b00222aadd29e91a3cf7db14d604f1a958
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-17 21:16:31 +0100

    Create hot.module.ts

---
+++ src/pages/TabsSections/hot/hot.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { HotPage } from './hot';
+import { ComponentsModule } from '../../../components/components.module';
+import { TranslateModule } from "@ngx-translate/core";
+
+@NgModule({
+    declarations: [
+        HotPage,
+    ],
+    imports: [
+        ComponentsModule,
+        IonicPageModule.forChild(HotPage),
+        TranslateModule.forChild()
+    ],
+})
+export class HotPageModule {}
