3ac61a6ff2224afe5c48f3553e9c36843e487e68
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-17 20:56:25 +0100

    Create wallet.module.ts

---
+++ src/pages/MenuPages/wallet/wallet.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { WalletPage } from './wallet';
+import { ComponentsModule } from '../../../components/components.module';
+import { TranslateModule } from "@ngx-translate/core";
+
+@NgModule({
+    declarations: [
+        WalletPage,
+    ],
+    imports: [
+        ComponentsModule,
+        IonicPageModule.forChild(WalletPage),
+        TranslateModule.forChild()
+    ],
+})
+export class WalletPageModule {}
