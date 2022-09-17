0a3099b8d1118bc2e00926e5ea8f3077f280fc77
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-17 21:15:35 +0100

    Update search.module.ts

---
+++ src/pages/search/search.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SearchPage } from './search';
+import { ComponentsModule } from '../../components/components.module';
+import { TranslateModule } from "@ngx-translate/core";
+
+@NgModule({
+    declarations: [
+        SearchPage,
+    ],
+    imports: [
+        ComponentsModule,
+        IonicPageModule.forChild(SearchPage),
+        TranslateModule.forChild()
+    ],
+})
+export class SearchPageModule {}
