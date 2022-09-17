b19c4c5981c219b2627eb4eb74293c5fe5a9ff79
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-17 21:31:26 +0100

    Create trend.module.ts

---
+++ src/pages/TabsSections/trend/trend.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { TrendPage } from './trend';
+import { ComponentsModule } from '../../../components/components.module';
+import { TranslateModule } from "@ngx-translate/core";
+
+@NgModule({
+    declarations: [
+        TrendPage,
+    ],
+    imports: [
+        ComponentsModule,
+        IonicPageModule.forChild(TrendPage),
+        TranslateModule.forChild()
+    ],
+})
+export class TrendPageModule {}
