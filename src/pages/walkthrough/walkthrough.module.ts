4aeaa2b8f7cf3691dc58d7ac77cc270e2bf39e66
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-17 15:21:34 +0100

    Create walkthrough.module.ts

---
+++ src/pages/walkthrough/walkthrough.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { WalkthroughPage } from './walkthrough';
+import { TranslateModule } from "@ngx-translate/core";
+
+@NgModule({
+    declarations: [
+        WalkthroughPage,
+    ],
+    imports: [
+        IonicPageModule.forChild(WalkthroughPage),
+        TranslateModule.forChild()
+    ],
+})
+export class WalkthroughPageModule {}
