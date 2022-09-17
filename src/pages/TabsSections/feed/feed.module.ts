de0f112b37540f472442a40e95137ca657d0ed33
hicham outaleb <31773165+Hi1talib1World@users.noreply.github.com>
2022-09-17 21:16:33 +0100

    Update feed.module.ts

---
+++ src/pages/TabsSections/feed/feed.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { FeedPage } from './feed';
+import { ComponentsModule } from '../../../components/components.module';
+import { TranslateModule } from "@ngx-translate/core";
+
+@NgModule({
+    declarations: [
+        FeedPage,
+    ],
+    imports: [
+        ComponentsModule,
+        IonicPageModule.forChild(FeedPage),
+        TranslateModule.forChild()
+    ],
+})
+export class FeedPageModule {}
