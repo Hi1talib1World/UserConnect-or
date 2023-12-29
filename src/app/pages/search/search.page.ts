815c164198b8605cab7da9819472dc3933e55522
Hi1talib1World <hicham.outaleb@Taalim.ma>
2023-12-29 08:22:05 +0100

    Update search.page.ts

--- src/app/pages/search/search.page.ts
+++ src/app/pages/search/search.page.ts
@@ -1,27 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-
-import { ApiService } from '../../services/api.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-search',
-  templateUrl: './search.page.html',
-  styleUrls: ['./search.page.scss'],
+  templateUrl: 'search.page.html',
+  styleUrls: ['search.page.scss'],
 })
 export class SearchPage implements OnInit {
-    friends = [];
-    currentSearchQuery: string = "";
-    search: any;
+  friendsData: any;
+  currentSearchQuery = '';
+  filteredFriends = [];
 
-  constructor(
-    private apiService: ApiService,
-    public modalController: ModalController
-  ) { }
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    
+    // Fetch JSON data from a file path
+    this.http.get('../../assets/data/data.json').subscribe((data: any) => {
+      this.friendsData = data;
+    });
   }
 
-  
-
+  searchQueryChanged(query: string) {
+    // Filter friends based on the current search query
+    this.filteredFriends = this.friendsData.friends.filter(
+      (friend) => friend.name.toLowerCase().includes(query.toLowerCase())
+    );
+  }
 }
